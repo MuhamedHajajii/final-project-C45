e8bc552983c383579d2ad24a0ce235e36735b08b
MuhamedHajajii <MuhamedHajajii@outlook.com>
2025-09-29 11:41:03 +0300

    test

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   prod: true,
   apiUrl: 'https://ecommerce.routemisr.com/api/v1/',
-  googleClientId: 'YOUR_GOOGLE_CLIENT_ID_HERE',
+  googleClientId:
+    '122459233200-g9gig0ce5h6c8mok2sj57a9o07e83ffb.apps.googleusercontent.com',
 };
